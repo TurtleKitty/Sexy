fbe3183e23f33ae30049170663ca412e2100f370
TurtleKitty <joshua@anwu.org>
2016-01-28 05:00:36 -0600

    First tentative Go steps

---
+++ compiler/golang/src/sexy/interface.go
@@ -0,0 +1,15 @@
+package sexy
+
+type SxyObj interface {
+    Answers     SxyObj
+    Autos       SxyObj
+    Default     SxyObj
+    Messages    SxyObj
+    Resends     SxyObj
+    Send        SxyObj
+    ToBool      SxyObj
+    ToText      SxyObj
+    Type        SxyObj
+    View        SxyObj
+}
+
---
+++ compiler/golang/src/sexy/pair.go
@@ -0,0 +1,47 @@
+package sexy
+
+type Pair struct {
+    head SxyObj
+    tail SxyObj
+}
+
+func (p Pair) Answers (m SxyObj) SxyObj {
+    // return messages.has? m
+}
+
+func (p Pair) Autos SxyObj {
+    // return autos
+}
+
+func (p Pair) Default SxyObj {
+    // fn to accept ints and return vals
+}
+
+func (p Pair) Messages SxyObj {
+    // ...
+}
+
+func (p Pair) Resends SxyObj {
+    // return ()
+}
+
+func (p Pair) Send (m SxyObj) SxyObj {
+    // the money shot
+}
+
+func (p Pair) ToBool SxyObj {
+    // to-bool
+}
+
+func (p Pair) ToText SxyObj {
+    // ...
+}
+
+func (p Pair) Type SxyObj {
+    // return 'pair
+}
+
+func (p Pair) View SxyObj {
+    // return (x . y) ?
+}
+
