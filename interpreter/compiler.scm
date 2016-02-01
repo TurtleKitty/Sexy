e4fdf0da720cc4dc67d6c3dfa65f5356e79183e5
TurtleKitty <joshua@anwu.org>
2016-01-31 20:34:29 -0600

    Better error plus test for multibody lets

--- interpreter/compiler.scm
+++ interpreter/compiler.scm
@@ -89,7 +89,7 @@
                                 code
                                 (lambda (v)
                                     (if (eq? not-found v)
-                                        (err (cons 'undefined-symbol code) cont)
+                                        (err (sexy-object `(name undefined-symbol form ,code to-text "Name not defined.") #f #f #f) cont)
                                         (cont v)))
                                 err))))))
         pass))
